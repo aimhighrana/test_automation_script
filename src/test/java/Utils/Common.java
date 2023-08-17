83f0afdd926331a1a51f30cb3f432df4277d126c
vishalkiwiqa <vishal.sinha@kiwiqa.com>
2023-08-17 18:08:58 +0530

    Added test data for workflow 11

--- src/test/java/Utils/Common.java
+++ src/test/java/Utils/Common.java
@@ -530,7 +530,7 @@ public class Common extends Locators {
         int rownum=0;
         ArrayList<String> code = new  ArrayList<String>();
         formatter = new DataFormatter();
-        FileInputStream    fin = new FileInputStream("Workflow11.xlsx");
+        FileInputStream    fin = new FileInputStream("Test_data1.xlsx");
             ExcelWBook = new XSSFWorkbook(fin);
             ExcelWSheet = ExcelWBook.getSheet(sheetname);
         try {
